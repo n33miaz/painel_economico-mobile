aa351552396dc2ec425e68efe434658e1a62617c
n33miaz <ncormino@gmail.com>
2025-11-17 16:10:36 -0300

    refactor(About): add expo-font, expo-linking and expo-constants

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import Routes from "./src/routes";
+import { ActivityIndicator, View } from "react-native";
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <>
       <StatusBar style="auto" />
--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Linking,
+  Platform,
+} from "react-native";
+import Constants from "expo-constants";
+import { Ionicons } from "@expo/vector-icons";
+
+const LINKEDIN_URL = "https://www.linkedin.com/in/neemiasmanso/";
 
 export default function AboutScreen() {
+  const handlePress = () => {
+    Linking.openURL(LINKEDIN_URL);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tela Sobre</Text>
+      <Ionicons name="information-circle-outline" size={80} color="#3498db" />
+      <Text style={styles.title}>Painel Econômico BR</Text>
+      <Text style={styles.subtitle}>
+        Versão {Constants.expoConfig?.version}
+      </Text>
+      <Text style={styles.description}>
+        Aplicativo desenvolvido por Neemias Cormino Manso.
+      </Text>
+      <Button title="Ver meu LinkedIn" onPress={handlePress} />
     </View>
   );
 }
@@ -14,8 +37,26 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 20,
+    paddingTop: Constants.statusBarHeight + 20,
   },
   title: {
-    fontSize: 22,
+    fontSize: 28,
+    fontFamily: "Roboto_700Bold",
+    marginVertical: 15,
+    textAlign: "center",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "gray",
+    fontFamily: "Roboto_400Regular",
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: "center",
+    marginHorizontal: 20,
+    marginBottom: 30,
+    fontFamily: "Roboto_400Regular",
   },
 });
